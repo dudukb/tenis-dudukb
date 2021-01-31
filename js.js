49a8e7eace5042b47fab583ed3bbcb8f98760f28
Eduardo Santos <76977508+dudukb@users.noreply.github.com>
2021-01-31 17:08:05 -0300

    Update js.js

--- js.js
+++ js.js
@@ -1,13 +1,12 @@
 const tenisGreen = document.querySelector('#green');
 const tenisBlue = document.querySelector('#blue');
+
 const tenisRed = document.querySelector('#red');
 const button = document.querySelector('.button');
 const divTamanho = document.querySelector('.tamanho');
-
 tenisBlue.onclick = changeToBlue;
 tenisGreen.onclick = changeToGreen;
 tenisRed.onclick = changeToRed;
-
 function changeToBlue() {
   button.style.backgroundColor = '#3864ff';
   document.querySelector('#img').src ='https://i.postimg.cc/43K2sD3y/tenis-blue.png';
